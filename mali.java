d33f28b33481b736832e3f8ece9174f66a4ba24d
darrenchooji <darrenchoojuninn@gmail.com>
2024-12-18 21:44:43 +0800

    Mali - Updated

--- mali.java
+++ mali.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.Reader;
 
 public class mali {
     public static void main(String[] args) throws IOException {
